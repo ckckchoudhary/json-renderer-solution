084ff976a1208ceeca88a40e1fbf8dbad2f97e5b
Chetan Kumar <chetan.kumar@curefit.com>
2021-10-06 00:06:49 +0530

    Bug: fix double click requied to open accordion after closed due to end of child

--- src/Routes/NestedRenderer.tsx
+++ src/Routes/NestedRenderer.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UiContext } from "../Contexts/UiContexts";
 import { NestedJson } from "../Models/NestedJson";
 import { Accordion } from "../widgets/Accordion/Accordion";
+
 
 
 interface NestedRendererProps {
@@ -9,20 +11,28 @@ interface NestedRendererProps {
 }
 
 export const NestedRenderer: React.FC<NestedRendererProps> = ({ data, fetchCountiesForContinent }) => {
+    const { shouldAllAccordionsBeClosed, setHideAllAccordionsFlag, unSetHideAllAccordionsFlag } = useContext(UiContext);
 
     const handleAccordionToggle = (isOpen: boolean, children: Array<NestedJson>, code?: string,) => {
         if (isOpen && code && children?.length === 0) {
             fetchCountiesForContinent(code);
         }
+        if (!code && (!children || children.length === 0)) {
+            setHideAllAccordionsFlag();
+        } else {
+            unSetHideAllAccordionsFlag();
+        }
     }
+
 
     return <div>
         {
             data.map((currentData) => {
                 const { children, name, code } = currentData;
+                const shouldShowCaret = Boolean(code || (children && children.length > 0));
                 // passing name as key, assuming that the names will be unique among siblings
-                return (<Accordion key={name} title={name} code={code}
-                    handleClick={(isOpen: boolean) => handleAccordionToggle(isOpen, children, code)} >
+                return (<Accordion key={name} title={name} shouldShowCaret={shouldShowCaret}
+                    isAllClosed={shouldAllAccordionsBeClosed} handleClick={(isOpen: boolean) => handleAccordionToggle(isOpen, children, code)} >
                     {(currentData.children && currentData.children.length > 0) &&
                         <NestedRenderer data={children} fetchCountiesForContinent={fetchCountiesForContinent} />
                     }
--- src/widgets/Accordion/Accordion.tsx
+++ src/widgets/Accordion/Accordion.tsx
@@ -1,47 +1,39 @@
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 import "./Accordion.css";
 import caret from "../../assets/icons/caret.png";
-import { UiContext } from "../../Contexts/UiContexts";
 
 interface AccordionProps {
     title: string;
     handleClick(localIsAccordionOpenFlag: boolean): void;
-    code?: string;
+    isAllClosed: boolean;
+    shouldShowCaret?:boolean
 }
 
 
-export const Accordion: React.FC<AccordionProps> = ({ children, title, handleClick, code }) => {
+export const Accordion: React.FC<AccordionProps> = ({ children, title, handleClick, isAllClosed, shouldShowCaret }) => {
 
-    const { shouldAllAccordionsBeClosed, setHideAllAccordionsFlag, unSetHideAllAccordionsFlag } = useContext(UiContext);
     const [localIsAccordionOpenFlag, setlocalIsAccordionOpenFlag] = useState(false);
 
 
     const toggleAccordion = (event: React.MouseEvent) => {
         event.stopPropagation();
-        if (code || children) {
-            if (shouldAllAccordionsBeClosed) {
-                unSetHideAllAccordionsFlag();
-            } else {
-                handleClick(!localIsAccordionOpenFlag);
-            }
-            setlocalIsAccordionOpenFlag(!localIsAccordionOpenFlag);
-        } else {
-            setHideAllAccordionsFlag();
-        }
+        handleClick(!localIsAccordionOpenFlag);
+        setlocalIsAccordionOpenFlag(!localIsAccordionOpenFlag);
     }
-  
-    React.useEffect(()=>{
-        setlocalIsAccordionOpenFlag(false);
-    },[shouldAllAccordionsBeClosed])
 
-    const isOpen = localIsAccordionOpenFlag && !shouldAllAccordionsBeClosed;
+    React.useEffect(() => {
+        if (isAllClosed) {
+            setlocalIsAccordionOpenFlag(false);
+        }
+    }, [isAllClosed])
+
     return (
-        <div className={`accordionContainer${(isOpen && children) ? " open" : ""}`} onClick={toggleAccordion}>
+        <div className={`accordionContainer${(localIsAccordionOpenFlag && children) ? " open" : ""}`} onClick={toggleAccordion}>
             <div className="accordionHeader">
-                <img src={caret} className="caret" alt="close-accordion" />
+                {shouldShowCaret &&  <img src={caret} className="caret" alt="close-accordion" />}
                 <span>{title}</span>
             </div>
-            {(isOpen) &&
+            {(localIsAccordionOpenFlag) &&
                 <>{children}</>
             }
         </div>)
